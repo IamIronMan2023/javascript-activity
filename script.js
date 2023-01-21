bcb36446ffe9de2902fef819f73818ecfe1bfbdb
jkujo20 <jkujo20@yahoo.com>
2023-01-21 10:37:43 +0800

    First Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+let input = document.getElementById("input");
+
+function reverse_a_text(text) {
+  //Add code here to reverse the string
+}
+
+function display() {
+  //Add code here to display text to webpage
+}
+
+btn.addEventListener("click", display);
