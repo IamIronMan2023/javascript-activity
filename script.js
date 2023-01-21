5d9767f11eac96df2f8a1dbf6114a79791ccfe24
jkujo20 <jkujo20@yahoo.com>
2023-01-21 12:53:08 +0800

    Add Pascal Case code

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-let btn = document.getElementById("btn");
-let input = document.getElementById("input");
+let btnReverse = document.getElementById("btnReverse");
+let inputReverse = document.getElementById("inputReverse");
 
-function reverse_a_text(text) {
-  //Add code here to reverse the string
+let btnPascalCase = document.getElementById("btnPascalCase");
+let inputPascalCase = document.getElementById("inputPascalCase");
+
+function reverseAText(text) {
+  return text.split("").reverse().join("");
 }
 
-function display() {
-  //Add code here to display text to webpage
+function pascalCaseAText(text) {
+  let arrText = text.split(" ");
+  let arrResult = [];
+  let s;
+  for (let i = 0; i < arrText.length; i++) {
+    s = arrText[i];
+    arrResult.push(
+      s.charAt(0).toUpperCase() + s.substring(1, s.length).toLowerCase()
+    );
+    arrResult.push(" ");
+  }
+
+  return arrResult.join("");
 }
 
-btn.addEventListener("click", display);
+function runPascalCaseText() {
+  let text = inputPascalCase.value;
+  text = pascalCaseAText(text);
+  document.getElementById("demoPascalCase").innerHTML = text;
+}
+
+function runReverseText() {
+  let text = inputReverse.value;
+  text = reverseAText(text);
+  document.getElementById("demoReverse").innerHTML = text;
+}
+
+btnReverse.addEventListener("click", runReverseText);
+btnPascalCase.addEventListener("click", runPascalCaseText);
